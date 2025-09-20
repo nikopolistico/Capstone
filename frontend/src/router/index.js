1afb4c5c1bc346c2e9082339fdee9111830143dc
nikopolistico <nik2polistico@gmail.com>
2025-09-20 21:05:17 +0800

    new

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import TanodLogin from '@/views/barangay/Tanod/TanodLogin.vue'
 import TanodInterface from '@/views/barangay/Tanod/TanodInterface.vue'
 import BarangayView from '@/views/barangay/BarangayView.vue'
@@ -10,9 +10,9 @@ import ProtoReporting from '@/views/user/ProtoReporting.vue'
 import BarangayProto from '@/views/barangay/BarangayProto.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/assigning-tanod', name: 'assigning-tanod', component: AssigningTanod },
+    { path: '/assigning-tanod', name: 'assigning-tanod', component: AssigningTanod },
     { path: '/unverified-verified', name: 'unverified-verified', component: UnVerified_Verified },
     { path: '/tanod-login', name: 'tanod-login', component: TanodLogin },
     { path: '/tanod-interface', name: 'tanod-interface', component: TanodInterface },
@@ -20,8 +20,8 @@ const router = createRouter({
     { path: '/', name: 'started', component: StartedViews },
     { path: '/login-admin', name: 'login-admin', component: LoginAdmin },
     { path: '/proto-reporting', name: 'proto-reporting', component: ProtoReporting },
-    { path: '/reports-list', name: 'reports-list', component: BarangayProto }
-  ]
+    { path: '/reports-list', name: 'reports-list', component: BarangayProto },
+  ],
 })
 
 export default router
